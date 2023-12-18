0e249646e79775d728f3e7bbd7d67a1b7d7e7ae0
manoj78943 <manojbittu161@gmail.com>
2023-12-18 20:44:19 +0530

    added collected static

--- TodoAlgoBulls/settings.py
+++ TodoAlgoBulls/settings.py
@@ -130,6 +130,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Replace 'staticfiles' with your static folder name
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
